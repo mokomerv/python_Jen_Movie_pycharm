047238e2a7d5b4df883ce1bd5019f8d6bad1c6bb
mokomerv <92145105+mokomerv@users.noreply.github.com>
2023-12-21 14:50:00 +1300

    Create 00_MMF_bas_v1.py

---
+++ 00_MMF_bas_v1.py
@@ -0,0 +1,28 @@
+# import statements
+
+
+# functions go here
+
+
+# ******** Main Routine ********
+
+# Set up dictionaries/ lists needed to hold data
+
+# Ask user if they have used the program before and show instructions if necessary
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # Ask for payment method (and apply surcharge if necessary)
+
+
+    # CalculateTotal sales and profit
