fe414849e31671d6146c1c207df618bdb1c3e9be
mokomerv <92145105+mokomerv@users.noreply.github.com>
2024-01-04 00:35:11 +1300

    Create 01_ticket_loop_v1.py

--- 01_ticket_loop_v1.py
+++ 01_ticket_loop_v1.py
@@ -6,11 +6,14 @@ count = 0
 MAX_TICKETS = 5
 
 while name != "xxx" and count < MAX_TICKETS:
+    print("You have {} seats "
+          "left".format(MAX_TICKETS - count))
 
     # Get details
     name = input("What is your name?:  ")
     count += 1
-    print(count, " tickets have been purchased")
 
-    if count < MAX_TICKETS:
-        print("There are no more tickets")
+if count == MAX_TICKETS:
+    print("You have sold all the tickets!")
+else:
+    print("You have sold {} tickets. ")
